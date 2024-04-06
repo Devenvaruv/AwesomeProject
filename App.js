bbd5698f5f0dd90fed37b445e60f8a3d01c8cf8e
your_github_username <devenvaruv@gmail.com>
2024-04-06 02:03:40 -0700

    react native code

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as FileSystem from 'expo-file-system';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await camera.takePictureAsync(options);
+      const savePath = `${FileSystem.documentDirectory}${Date.now()}.jpg`;
+      await FileSystem.writeAsStringAsync(savePath, data.base64, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+      console.log('Photo saved at', savePath);
+    }
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={type} ref={(ref) => setCamera(ref)}>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Flip Image"
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          />
+          <Button title="Take Picture" onPress={takePicture} />
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -14,7 +59,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
   },
 });
